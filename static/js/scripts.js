62188afc35d24de8892b3dbd0aaf8df351f8acf4
krahser <aldo.vizcaino87@gmail.com>
2015-03-10 18:56:46 -0300

    Add content

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,24 +1,23 @@
-
 $(document).ready(function(){/* google maps -----------------------------------------------------*/
 google.maps.event.addDomListener(window, 'load', initialize);
 
 function initialize() {
 
   /* position Amsterdam */
-  var latlng = new google.maps.LatLng(52.3731, 4.8922);
+    var latlng = new google.maps.LatLng(-34.9205284,-57.9531702);
 
   var mapOptions = {
     center: latlng,
     scrollWheel: false,
     zoom: 13
   };
-  
+
   var marker = new google.maps.Marker({
     position: latlng,
     url: '/',
     animation: google.maps.Animation.DROP
   });
-  
+
   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   marker.setMap(map);
 
