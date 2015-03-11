7e69c52a09dc520b84403762bb047cca69e04c31
krahser <aldo.vizcaino87@gmail.com>
2015-03-11 01:49:44 -0300

    drawing manager to add markers

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -12,15 +12,41 @@ function initialize() {
     zoom: 13
   };
 
+
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
   var marker = new google.maps.Marker({
     position: latlng,
     url: '/',
     animation: google.maps.Animation.DROP
   });
 
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  marker.setMap(map);
+    // var drawingManager = new google.maps.drawing.DrawingManager({
+    //     drawingMode: google.maps.drawing.OverlayType.MARKER,
+    //     drawingControl: true,
+    //     drawingControlOptions: {
+    //         position: google.maps.ControlPosition.TOP_CENTER,
+    //         drawingModes: [
+    //             google.maps.drawing.OverlayType.MARKER,
+    //         ]}
+    // });
 
+    var drawingManager = new google.maps.drawing.DrawingManager({
+        drawingMode: google.maps.drawing.OverlayType.MARKER,
+        drawingControl: true,
+        drawingControlOptions: {
+            position: google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: [
+                google.maps.drawing.OverlayType.MARKER
+            ]
+        },
+        markerOptions: {
+            editable: true,
+            draggable: true
+        }
+    });
+
+  drawingManager.setMap(map);
 };
 /* end google maps -----------------------------------------------------*/
 });
